33b6c8863cded4f1f81a6dd144d744ccf85b0e43
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-20 01:54:51 +0300

    added accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/',views.register, name ='register' ),
     path('login/',auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'index.html'), name='logout'),
+    path('profile/',views.user_info,name='profile'),
+    # path('hood/',views.area,name='hood')
 ]
 
 if settings.DEBUG:
