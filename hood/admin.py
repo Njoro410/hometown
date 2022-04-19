cb1bc58e36616a569e9fb4dbb81eb3838e6191fa
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-20 02:03:38 +0300

    registered hood models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Business,Hospitals,Police,Posts,Tags
 # Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(Business)
+admin.site.register(Hospitals)
+admin.site.register(Police)
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Tags)
